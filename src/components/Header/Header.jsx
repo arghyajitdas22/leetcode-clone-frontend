7198059913d767b1a866da6fb75ee4b60e199c97
Arghyajit <arghyajjitdas22@gmail.com>
2023-12-15 12:33:15 +0530

    #1: tailwind added and project setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      {/* leetcode svg problems and login/register or signout */}
+      Header
+    </div>
+  );
+};
+
+export default Header;
